35153b696ee480614c3ce95eae5e33ae5e6a8e0c
Fuad Saud <fuad.saud@nubank.com.br>
2021-06-08 21:44:17 -0300

    Cleanup

--- keyboards/ergodash/rev1/keymaps/fuad/keymap.c
+++ keyboards/ergodash/rev1/keymaps/fuad/keymap.c
@@ -100,7 +100,7 @@ enum user_layers {
 #define M_MNXT KC_MFFD
 #define M_MPRV KC_MRWD
 
-#define G_PTT LGUI(KC_BSLS)
+#define G_PTT KC_F20
 #define I_PTT KC_F20
 #define M_PTT ___x___
 
@@ -155,15 +155,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* i3 QWERTY LAYER
    *
    *                     ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐                 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐
-   *                     │  `  │  1  │  2  │  3  │  4  │  5  │HOME │                 │PGUP │  6  │  7  │  8  │  9  │  0  │  \  │
+   *                     │  `  │  1  │  2  │  3  │  4  │  5  │  ▶  │                 │LOCK │  6  │  7  │  8  │  9  │  0  │  \  │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤                 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   *                     │ TAB │  Q  │  W  │  E  │  R  │  T  │END  │                 │PGDN │  Y  │  U  │  I  │  O  │  P  │  '  │
+   *                     │ TAB │  Q  │  W  │  E  │  R  │  T  │HOME │                 │PGUP │  Y  │  U  │  I  │  O  │  P  │  '  │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤                 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   *      Tap for ESC -- │ CTL │  A  │  S  │  D  │  F  │  G  │VOL- │                 │VOL+ │  H  │  J  │  K  │  L  │  ;  │SUPER│ -- Tap for ↵
+   *      Tap for ESC -- │ CTL │  A  │  S  │  D  │  F  │  G  │ END │                 │PGDN │  H  │  J  │  K  │  L  │  ;  │SUPER│ -- Tap for ↵
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┐     ┌─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   *        Tap for ( ---│SHIFT│  Z  │  X  │  C  │  V  │  B  │  ▶  │     │     │     │  ↵  │  N  │  M  │  ,  │  .  │  /  │SHIFT│--- Tap for )
+   *        Tap for ( ---│SHIFT│  Z  │  X  │  C  │  V  │  B  │VOL- │     │     │     │VOL+ │  N  │  M  │  ,  │  .  │  /  │SHIFT│--- Tap for )
    *                     ├─────┼─────┼─────┼─────┴─────┼─────┼─────┤     │     │     ├─────┼─────┼─────┴─────┼─────┼─────┼─────┤
-   *                     │LEADR│SUPER│ ALT │    GUI    │LOWER│  ␣  │  ␣  │     │ ⌫   │ ⌫   │RAISE│    GUI    │ MEH │ PTT │META │
+   *                     │LEADR│SUPER│ ALT │  CONTROL  │SUPER│LOWER│  ␣  │     │ ⌫   │RAISE│  ↵  │   HYPER   │ MEH │ PTT │META │
    *                     └─────┴─────┴─────┴───────────┴─────┴─────┴─────┘     └─────┴─────┴─────┴───────────┴─────┴─────┴─────┘
    *                              |           |           |                                   |     |           |
    *                              |           |        Tap for -                         Tap for =  |           |
@@ -174,56 +174,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [LAYER_QWERTY_I3] = LAYOUT(
-    KC_GRV   , KC_1    , KC_2    , KC_3    , KC_4  , KC_5    , KC_HOME          ,           KC_PGUP , KC_6    , KC_7 , KC_8    , KC_9    , KC_0    , KC_BSLS,
-    KC_TAB   , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T    , KC_END           ,           KC_PGDN , KC_Y    , KC_U , KC_I    , KC_O    , KC_P    , KC_QUOT,
-    LCTL_ESC , KC_A    , KC_S    , KC_D    , KC_F  , KC_G    , G_VOLD           ,           G_VOLU  , KC_H    , KC_J , KC_K    , KC_L    , KC_SCLN , G_ENTER,
-    KC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B    , I_MPLY           ,           KC_ENT  , KC_N    , KC_M , KC_COMM , KC_DOT  , KC_SLSH , KC_RSPC,
-    KC_LEAD  , KC_LGUI , KC_LALT ,      I_C_BRO    , I_LOWER , KC_SPC  , KC_SPC , KC_BSPC , KC_BSPC , I_RAISE ,    KC_LEFT     , KC_DOWN , I_PTT   , META
+    KC_GRV   , KC_1    , KC_2    , KC_3    , KC_4  , KC_5    , I_MPLY           ,           I_LOCK  , KC_6    , KC_7 , KC_8    , KC_9    , KC_0    , KC_BSLS,
+    KC_TAB   , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T    , KC_HOME          ,           KC_PGUP , KC_Y    , KC_U , KC_I    , KC_O    , KC_P    , KC_QUOT,
+    LCTL_ESC , KC_A    , KC_S    , KC_D    , KC_F  , KC_G    , KC_END           ,           KC_PGDN , KC_H    , KC_J , KC_K    , KC_L    , KC_SCLN , G_ENTER,
+    KC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B    , G_VOLD           ,           G_VOLU  , KC_N    , KC_M , KC_COMM , KC_DOT  , KC_SLSH , KC_RSPC,
+    KC_LEAD  , KC_LGUI , KC_LALT ,      I_C_BRO    , I_C_BRO , I_LOWER , KC_SPC , KC_BSPC , I_RAISE , HYP_BRC ,    HYP_BRC     , MEH_LEA , I_PTT   , META
+  ),
+
+  [LAYER_QWERTY_MACOS] = LAYOUT(
+    KC_GRV   , KC_1    , KC_2    , KC_3    , KC_4  , KC_5    , M_MPLY           ,           M_LOCK  , KC_6    , KC_7 , KC_8    , KC_9    , KC_0    , KC_BSLS,
+    KC_TAB   , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T    , KC_HOME          ,           KC_PGUP , KC_Y    , KC_U , KC_I    , KC_O    , KC_P    , KC_QUOT,
+    LCTL_ESC , KC_A    , KC_S    , KC_D    , KC_F  , KC_G    , KC_END           ,           KC_PGDN , KC_H    , KC_J , KC_K    , KC_L    , KC_SCLN , M_ENTER,
+    KC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B    , M_VOLD           ,           M_VOLU  , KC_N    , KC_M , KC_COMM , KC_DOT  , KC_SLSH , KC_RSPC,
+    KC_LEAD  , KC_LGUI , KC_LALT ,      M_C_BRO    , M_C_BRO , M_LOWER , KC_SPC , KC_BSPC , M_RAISE , HYP_BRC ,    HYP_BRC     , MEH_LEA , M_PTT   , META
   ),
 
   /* LOWER LAYER
    *
    *                     ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐                 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐
-   *                     │     │ F1  │ F2  │ F3  │ F4  │ F5  │     │                 │PGUP │ F6  │ F7  │ F8  │ F9  │ F10 │  \  │
+   *                     │     │ F1  │ F2  │ F3  │ F4  │ F5  │MUTE │                 │PGUP │ F6  │ F7  │ F8  │ F9  │ F10 │  \  │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤                 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    *                     │     │  !  │  @  │  #  │  $  │  %  │     │                 │PGDN │  ^  │  &  │  *  │  (  │  )  │  '  │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤                 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    *                     │     │  ◌̀  │  ◌̂  │  ◌̧  │  ◌́  │  ◌̃  │     │                 │VOL+ │     │  {  │  }  │     │  |  │     │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┐     ┌─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   *                     │     │     │     │     │PMAIL│WMAIL│  ▶  │     │     │     │  ↵  │     │  (  │  )  │     │  \  │     │
+   *                     │     │     │     │     │PMAIL│WMAIL│♪ PRV│     │     │     │♪ NXT│     │  (  │  )  │     │  \  │     │
    *                     ├─────┼─────┼─────┼─────┴─────┼─────┼─────┤     │     │     ├─────┼─────┼─────┴─────┼─────┼─────┼─────┤
-   *                     │     │     │     │           │|||||│  ␣  │  ␣  │     │ ⌦   │ ⌦   │     │           │     │     │     │
+   *                     │     │     │     │           │|||||│     │     │     │ DEL │     │     │           │     │     │     │
    *                     └─────┴─────┴─────┴───────────┴─────┴─────┴─────┘     └─────┴─────┴─────┴───────────┴─────┴─────┴─────┘
    */
 
   [LAYER_LOWER_I3] = LAYOUT(
-    _______  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , _______           ,           _______ , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10    , I_SS    ,
+    _______  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , G_MUTE            ,           _______ , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10    , I_SS    ,
     KC_TILD  , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , _______           ,           _______ , I_WIN_L , I_WIN_D , I_WIN_U , I_WIN_R , I_WIN_MAX , I_WSS   ,
-    _______  , G_GRAVE , G_CIRCU , G_CEDIL , G_ACUTE , G_TILDE , I_MPRV            ,           I_MNXT  , _______ , KC_LCBR , KC_RCBR , _______ , KC_PIPE   , I_SSS   ,
-    _______  , _______ , _______ , _______ , X_PMAIL , X_WMAIL , _______           ,           _______ , _______ , KC_LPRN , KC_RPRN , _______ , KC_BSLS   , _______ ,
-    _______  , _______ , _______ ,      _______      , _______ , _______ , _______ , _______ , _______ , _______ ,      _______      , _______ , _______   , _______
+    _______  , G_GRAVE , G_CIRCU , G_CEDIL , G_ACUTE , G_TILDE , _______           ,           _______ , _______ , KC_LCBR , KC_RCBR , _______ , KC_PIPE   , I_SSS   ,
+    _______  , _______ , _______ , _______ , X_PMAIL , X_WMAIL , I_MPRV            ,           I_MNXT  , _______ , KC_LPRN , KC_RPRN , _______ , KC_BSLS   , _______ ,
+    _______  , _______ , _______ ,      _______      , _______ , _______ , _______ , KC_DEL  , _______ , _______ ,      _______      , _______ , _______   , _______
   ),
 
   /* RAISE LAYER
    *
    *                     ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐                 ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐
-   *                     │     │ F11 │ F12 │ F13 │ F14 │ F15 │     │                 │     │ F16 │ F17 │ F18 │ F19 │ F20 │ SS  │
+   *                     │     │ F11 │ F12 │ F13 │ F14 │ F15 │MUTE │                 │     │ F16 │ F17 │ F18 │ F19 │ F20 │ SS  │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤                 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    *                     │     │     │WIN ←│WIN ↓│WIN ↑│WIN →│     │                 │     │     │W SWI│WSW P│WSW N│     │ SSS │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤                 ├─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   *                     │     │     │MON ←│MON ↓│MON ↑│MON →│♪ PRV│                 │♪ NXT│  ←  │  ↓  │  ↑  │  →  │     │ WSS │
+   *                     │     │     │MON ←│MON ↓│MON ↑│MON →│     │                 │     │  ←  │  ↓  │  ↑  │  →  │     │ WSS │
    *                     ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┐     ┌─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-   *                     │     │     │     │TAB ←│TAB →│     │     │     │     │     │     │     │BACK │ FWD │     │     │     │
+   *                     │     │     │     │TAB ←│TAB →│     │♪ PRV│     │     │     │♪ NXT│     │BACK │ FWD │     │     │     │
    *                     ├─────┼─────┼─────┼─────┴─────┼─────┼─────┤     │     │     ├─────┼─────┼─────┴─────┼─────┼─────┼─────┤
    *                     │     │     │     │           │     │     │     │     │     │     │|||||│           │     │     │     │
    *                     └─────┴─────┴─────┴───────────┴─────┴─────┴─────┘     └─────┴─────┴─────┴───────────┴─────┴─────┴─────┘
    */
 
   [LAYER_RAISE_I3] = LAYOUT(
-    _______  , KC_F11  , KC_F12  , KC_F13  , KC_F14  , KC_F15  , _______           ,           _______ , KC_F16  , KC_F17  , KC_F18  , KC_F19   , KC_F20    , I_SS    ,
+    _______  , KC_F11  , KC_F12  , KC_F13  , KC_F14  , KC_F15  , G_MUTE            ,           _______ , KC_F16  , KC_F17  , KC_F18  , KC_F19   , KC_F20    , I_SS    ,
     _______  , I_WMV_L , I_WMV_D , I_WMV_U , I_WMV_R , _______ , _______           ,           _______ , I_WIN_L , I_WIN_D , I_WIN_U , I_WIN_R  , I_WIN_MAX , I_WSS   ,
-    _______  , I_MON_L , I_MON_D , I_MON_U , I_MON_R , _______ , I_MPRV            ,           I_MNXT  , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , _______   , I_SSS   ,
-    _______  , _______ , _______ , G_GLEFT , G_GRIGH , _______ , _______           ,           _______ , _______ , G_BACK  , G_FRWD  , _______  , _______   , _______ ,
+    _______  , I_MON_L , I_MON_D , I_MON_U , I_MON_R , _______ , _______           ,           _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , _______   , I_SSS   ,
+    _______  , _______ , _______ , G_GLEFT , G_GRIGH , _______ , I_MPRV            ,           I_MNXT  , _______ , G_BACK  , G_FRWD  , _______  , _______   , _______ ,
     _______  , _______ , _______ ,      _______      , _______ , _______ , _______ , _______ , _______ , _______ ,      _______      , _______  , _______   , _______
   ),
 
