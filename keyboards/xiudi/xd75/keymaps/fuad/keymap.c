7cd2f3b2352e080e13852fbd6b4d72e3729cc7db
Fuad Saud <fuadfsaud@gmail.com>
2023-10-08 16:14:18 +0200

    Fix xd75 keymap

--- keyboards/xiudi/xd75/keymaps/fuad/keymap.c
+++ keyboards/xiudi/xd75/keymaps/fuad/keymap.c
@@ -1,18 +1,20 @@
-/* Copyright 2017 Fuad Saud
+/* Copyright 2023 Fuad
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "xd75.h"
+
+#include QMK_KEYBOARD_H
 
 enum user_layers {
   LAYER_QWERTY_I3,
@@ -50,37 +52,37 @@ enum user_keycodes {
 
 // mod-tap
 #define LCTL_ESC LCTL_T(KC_ESC)
-#define G_ENTER LGUI_T(KC_ENT)
-#define M_ENTER LCMD_T(KC_ENT)
+#define G_ENTER LGUI_T(KC_ENTER)
+#define M_ENTER LCMD_T(KC_ENTER)
 
-#define G_C_BRO LCTL_T(KC_LBRC)
-#define I_C_BRO LGUI_T(KC_LBRC)
-#define M_C_BRO LCMD_T(KC_LBRC)
-#define MEH_LEA MEH(KC_LEAD)
-#define HYP_BRC HYPR_T(KC_RBRC)
-#define GUI_BRC LGUI_T(KC_RBRC)
+#define G_C_BRO LCTL_T(KC_LEFT_BRACKET)
+#define I_C_BRO LGUI_T(KC_LEFT_BRACKET)
+#define M_C_BRO LCMD_T(KC_LEFT_BRACKET)
+#define MEH_LEA MEH(QK_LEAD)
+#define HYP_BRC HYPR_T(KC_RIGHT_BRACKET)
+#define GUI_BRC LGUI_T(KC_RIGHT_BRACKET)
 
 // app launcher
-#define G_LNCHR KC_LGUI
+#define G_LNCHR KC_LEFT_GUI
 #define I_LNCHR LGUI(KC_D)
-#define M_LNCHR LCMD(KC_SPC)
+#define M_LNCHR LCMD(KC_SPACE)
 
 // misc
-#define G_SS  KC_PSCREEN
-#define G_WSS LALT(KC_PSCREEN)
-#define G_SSS LSFT(KC_PSCREEN)
-#define I_SS  KC_PSCREEN
-#define I_WSS LALT(KC_PSCREEN)
-#define I_SSS LSFT(KC_PSCREEN)
+#define G_SS  KC_PRINT_SCREEN
+#define G_WSS LALT(KC_PRINT_SCREEN)
+#define G_SSS LSFT(KC_PRINT_SCREEN)
+#define I_SS  KC_PRINT_SCREEN
+#define I_WSS LALT(KC_PRINT_SCREEN)
+#define I_SSS LSFT(KC_PRINT_SCREEN)
 #define M_SS  LCMD(LSFT(KC_3))
 #define M_SSS LCMD(LSFT(KC_4))
 
-#define G_SPC KC_SPC
-#define G_BSP KC_BSPC
-#define I_SPC KC_SPC
-#define I_BSP KC_BSPC
-#define M_SPC KC_SPC
-#define M_BSP KC_BSPC
+#define G_SPC KC_SPACE
+#define G_BSP KC_BACKSPACE
+#define I_SPC KC_SPACE
+#define I_BSP KC_BACKSPACE
+#define M_SPC KC_SPACE
+#define M_BSP KC_BACKSPACE
 
 #define G_BACK LALT(KC_LEFT)
 #define G_FRWD LALT(KC_RIGHT)
@@ -89,41 +91,41 @@ enum user_keycodes {
 
 #define G_GLEFT LCTL(LSFT(KC_TAB))
 #define G_GRIGH LCTL(KC_TAB)
-#define M_GLEFT LCMD(LSFT(KC_LBRC))
-#define M_GRIGH LCMD(LSFT(KC_RBRC))
+#define M_GLEFT LCMD(LSFT(KC_LEFT_BRACKET))
+#define M_GRIGH LCMD(LSFT(KC_LEFT_BRACKET))
 
 #define G_LOCK LGUI(KC_L)
-#define I_LOCK LGUI(KC_BSLS)
-#define M_LOCK LCTL(LSFT(KC_POWER))
+#define I_LOCK LGUI(KC_BACKSLASH)
+#define M_LOCK LCTL(LSFT(KC_SYSTEM_POWER))
 
-#define G_VOLD KC_VOLD
-#define G_VOLU KC_VOLU
-#define G_MUTE KC_MUTE
-#define M_VOLD KC__VOLDOWN
-#define M_VOLU KC__VOLUP
-#define M_MUTE KC__MUTE
+#define G_VOLD KC_AUDIO_VOL_DOWN
+#define G_VOLU KC_AUDIO_VOL_UP
+#define G_MUTE KC_AUDIO_MUTE
+#define M_VOLD KC_AUDIO_VOL_DOWN
+#define M_VOLU KC_AUDIO_VOL_UP
+#define M_MUTE KC_AUDIO_MUTE
 
-#define G_MPLY LALT(KC_MPLY)
-#define G_MNXT LALT(KC_MNXT)
-#define G_MPRV LALT(KC_MPRV)
-#define I_MPLY KC_MPLY
-#define I_MNXT KC_MNXT
-#define I_MPRV KC_MPRV
-#define M_MPLY KC_MPLY
-#define M_MNXT KC_MFFD
-#define M_MPRV KC_MRWD
+#define G_MPLY LALT(KC_MEDIA_PLAY_PAUSE)
+#define G_MNXT LALT(KC_MEDIA_NEXT_TRACK)
+#define G_MPRV LALT(KC_MEDIA_PREV_TRACK)
+#define I_MPLY KC_MEDIA_PLAY_PAUSE
+#define I_MNXT KC_MEDIA_NEXT_TRACK
+#define I_MPRV KC_MEDIA_PREV_TRACK
+#define M_MPLY KC_MEDIA_PLAY_PAUSE
+#define M_MNXT KC_MEDIA_FAST_FORWARD
+#define M_MPRV KC_MEDIA_REWIND
 
-#define G_PTT LGUI(KC_BSLS)
+#define G_PTT LGUI(KC_BACKSLASH)
 #define I_PTT KC_F20
 #define M_PTT ___x___
 
 // diacritics
-#define G_GRAVE RALT(KC_GRV)
-#define G_ACUTE RALT(KC_QUOT)
+#define G_GRAVE RALT(KC_GRAVE)
+#define G_ACUTE RALT(KC_QUOTE)
 #define G_TILDE RALT(KC_TILDE)
 #define G_CIRCU RALT(KC_CIRCUMFLEX)
-#define G_CEDIL RALT(KC_COMM)
-#define M_GRAVE LALT(KC_GRV)
+#define G_CEDIL RALT(KC_COMMA)
+#define M_GRAVE LALT(KC_GRAVE)
 #define M_ACUTE LALT(KC_E)
 #define M_TILDE LALT(KC_N)
 #define M_CIRCU LALT(KC_I)
@@ -139,8 +141,8 @@ enum user_keycodes {
 #define G_WIN_RST LGUI(KC_DOWN)
 #define I_WIN_MAX LGUI(KC_F)
 #define I_WIN_RST LGUI(KC_F)
-#define M_WIN_MAX LCMD(LALT(KC_F))
-#define M_WIN_RST LCTL(LALT(KC_BSPC))
+#define M_WIN_MAX LGUI(LALT(KC_F))
+#define M_WIN_RST LCTL(LALT(KC_BACKSPACE))
 
 #define G_WMV_L LGUI(KC_LEFT)
 #define G_WMV_R LGUI(KC_RIGHT)
@@ -233,24 +235,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_GRV   , KC_1    , KC_2    , KC_3    , KC_4    , KC_5  , KC_MINS , KC_ESC  , KC_EQL  , KC_6  , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS },
     { KC_TAB   , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T  , KC_LBRC , KC_BSLS , KC_RBRC , KC_Y  , KC_U    , KC_I    , KC_O    , KC_P    , KC_QUOT },
     { LCTL_ESC , KC_A    , KC_S    , KC_D    , KC_F    , KC_G  , KC_HOME , G_LOCK  , KC_PGUP , KC_H  , KC_J    , KC_K    , KC_L    , KC_SCLN , G_ENTER },
-    { KC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B  , KC_END  , G_MUTE  , KC_PGDN , KC_N  , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSPC },
-    { KC_LEAD  , G_LNCHR , KC_LALT , G_C_BRO , G_LOWER , G_SPC , G_VOLD  , G_MPLY  , G_VOLU  , G_BSP , G_RAISE , HYP_BRC , MEH_LEA , G_PTT   , META    },
+    { SC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B  , KC_END  , G_MUTE  , KC_PGDN , KC_N  , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , SC_RSPC },
+    { QK_LEAD  , G_LNCHR , KC_LALT , G_C_BRO , G_LOWER , G_SPC , G_VOLD  , G_MPLY  , G_VOLU  , G_BSP , G_RAISE , HYP_BRC , MEH_LEA , G_PTT   , META    },
   },
 
   [LAYER_QWERTY_I3] = {
     { KC_GRV   , KC_1    , KC_2    , KC_3    , KC_4    , KC_5  , KC_MINS , KC_ESC  , KC_EQL  , KC_6  , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS },
     { KC_TAB   , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T  , KC_LBRC , KC_BSLS , KC_RBRC , KC_Y  , KC_U    , KC_I    , KC_O    , KC_P    , KC_QUOT },
     { LCTL_ESC , KC_A    , KC_S    , KC_D    , KC_F    , KC_G  , KC_HOME , I_LOCK  , KC_PGUP , KC_H  , KC_J    , KC_K    , KC_L    , KC_SCLN , G_ENTER },
-    { KC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B  , KC_END  , G_MUTE  , KC_PGDN , KC_N  , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSPC },
-    { KC_LEAD  , KC_LGUI , KC_LALT , I_C_BRO , I_LOWER , I_SPC , G_VOLD  , I_MPLY  , G_VOLU  , I_BSP , I_RAISE , GUI_BRC , MEH_LEA , I_PTT   , META    },
+    { SC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B  , KC_END  , G_MUTE  , KC_PGDN , KC_N  , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , SC_RSPC },
+    { QK_LEAD  , KC_LGUI , KC_LALT , I_C_BRO , I_LOWER , I_SPC , G_VOLD  , I_MPLY  , G_VOLU  , I_BSP , I_RAISE , GUI_BRC , MEH_LEA , I_PTT   , META    },
   },
 
   [LAYER_QWERTY_MACOS] = {
     { KC_GRV   , KC_1    , KC_2    , KC_3    , KC_4    , KC_5  , KC_MINS , KC_ESC  , KC_EQL  , KC_6  , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSLS },
     { KC_TAB   , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T  , KC_LBRC , KC_BSLS , KC_RBRC , KC_Y  , KC_U    , KC_I    , KC_O    , KC_P    , KC_QUOT },
     { LCTL_ESC , KC_A    , KC_S    , KC_D    , KC_F    , KC_G  , KC_HOME , M_LOCK  , KC_PGUP , KC_H  , KC_J    , KC_K    , KC_L    , KC_SCLN , M_ENTER },
-    { KC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B  , KC_END  , M_MUTE  , KC_PGDN , KC_N  , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSPC },
-    { KC_LEAD  , M_LNCHR , KC_LALT , M_C_BRO , M_LOWER , M_SPC , M_VOLD  , M_MPLY  , M_VOLU  , M_BSP , M_RAISE , HYP_BRC , MEH_LEA , M_PTT   , META    },
+    { SC_LSPO  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B  , KC_END  , M_MUTE  , KC_PGDN , KC_N  , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , SC_RSPC },
+    { QK_LEAD  , M_LNCHR , KC_LALT , M_C_BRO , M_LOWER , M_SPC , M_VOLD  , M_MPLY  , M_VOLU  , M_BSP , M_RAISE , HYP_BRC , MEH_LEA , M_PTT   , META    },
   },
 
   /* RAISE LAYER
@@ -289,8 +291,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { _______ , KC_F11  , KC_F12    , KC_F13    , KC_F14  , KC_F15  , _______ , _______ , _______ , KC_F16  , KC_F17  , KC_F18  , KC_F19   , KC_F20  , M_SS    },
     { _______ , M_WMV_L , M_WIN_RST , M_WIN_MAX , M_WMV_R , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______  , M_SSS   , M_WSS   },
     { _______ , M_MON_L , M_MON_D   , M_MON_U   , M_MON_R , _______ , M_GLEFT , _______ , M_GRIGH , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , _______ , _______ },
-    { _______ , _______ , _______   , M_GLEFT   , M_GRIGH , _______ , M_BACK  , KC_PAUS , M_FRWD  , _______ , M_BACK  , M_FRWD  , _______  , _______ , _______ },
-    { _______ , _______ , _______   , _______   , _______ , M_LNCHR , M_MPRV  , KC_SLCK , M_MNXT  , _______ , _______ , _______ , _______  , _______ , _______ },
+    { _______ , _______ , _______   , M_GLEFT   , M_GRIGH , _______ , M_BACK  , KC_BRMU , M_FRWD  , _______ , M_BACK  , M_FRWD  , _______  , _______ , _______ },
+    { _______ , _______ , _______   , _______   , _______ , M_LNCHR , M_MPRV  , KC_BRMD , M_MNXT  , _______ , _______ , _______ , _______  , _______ , _______ },
   },
 
 
@@ -338,7 +340,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* META LAYER
    *
    *                              ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
-   *                              │     │     │     │     │     │     │     │RESET│     │     │     │     │     │     │     │
+   *                              │     │     │     │     │     │     │     │BOOT │     │     │     │     │     │     │     │
    *                              ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
    *                              │     │     │     │     │     │     │     │DEBUG│     │     │     │ I3  │     │     │     │
    *                              ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
@@ -352,8 +354,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [LAYER_META] = {
-    { ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___  , RESET   , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ },
-    { ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___  , DEBUG   , ___x___ , ___x___ , ___x___ , D_I3    , ___x___ , ___x___ , ___x___ },
+    { ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___  , QK_BOOT , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ },
+    { ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___  , DB_TOGG , ___x___ , ___x___ , ___x___ , D_I3    , ___x___ , ___x___ , ___x___ },
     { ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , D_GNOME , RGB_HUI  , RGB_SAI , RGB_VAI , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ },
     { ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , RGB_HUD  , RGB_SAD , RGB_VAD , ___x___ , D_MACOS , ___x___ , ___x___ , ___x___ , ___x___ },
     { ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , RGB_RMOD , RGB_TOG , RGB_MOD , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ , ___x___ },
@@ -401,9 +403,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch(keycode) {
     case M_WSS:
       if (record->event.pressed) {
-        SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LSHIFT) SS_TAP(X_4) SS_UP(X_LGUI) SS_UP(X_LSHIFT));
-        _delay_ms(100);
-        SEND_STRING(SS_TAP(X_SPACE));
+        SEND_STRING(SS_DOWN(X_LEFT_GUI) SS_DOWN(X_LEFT_SHIFT) SS_TAP(X_4) SS_UP(X_LEFT_GUI) SS_UP(X_LEFT_SHIFT) SS_DELAY(100) SS_TAP(X_SPACE));
       }
       return false;
 
@@ -415,7 +415,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case X_WMAIL:
       if (record->event.pressed) {
-        SEND_STRING("fuad.saud@nubank.com.br");
+        SEND_STRING("fuad@ellipsus.com");
       }
       return false;
 
@@ -441,35 +441,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-LEADER_EXTERNS();
-
-void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
-  }
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (biton32(state)) {
+    case LAYER_QWERTY_GNOME:
+    case LAYER_QWERTY_I3:
+    case LAYER_QWERTY_MACOS:
+        rgblight_setrgb (0x4D,  0x19, 0x83);
+        break;
+    case LAYER_LOWER_GNOME:
+    case LAYER_LOWER_I3:
+    case LAYER_LOWER_MACOS:
+        rgblight_setrgb (0x00,  0x8F, 0x60);
+        break;
+    case LAYER_RAISE_GNOME:
+    case LAYER_RAISE_I3:
+    case LAYER_RAISE_MACOS:
+        rgblight_setrgb (0xF0,  0x78, 0x46);
+        break;
+    case LAYER_META:
+        rgblight_setrgb (0xFF,  0x00, 0x00);
+        break;
+    default: //  for any other layers, or the default layer
+        rgblight_setrgb (0x00,  0xFF, 0xFF);
+        break;
+    }
+  return state;
 }
-
-/* uint32_t layer_state_set_user(uint32_t state) { */
-/*     switch (biton32(state)) { */
-/*     case LAYER_QWERTY_GNOME: */
-/*     case LAYER_QWERTY_MACOS: */
-/*         rgblight_setrgb (0x4D,  0x19, 0x83); */
-/*         break; */
-/*     case LAYER_LOWER_GNOME: */
-/*     case LAYER_LOWER_MACOS: */
-/*         rgblight_setrgb (0x00,  0x8F, 0x60); */
-/*         break; */
-/*     case LAYER_RAISE_L: */
-/*     case LAYER_RAISE_MACOS: */
-/*         rgblight_setrgb (0xF0,  0x78, 0x46); */
-/*         break; */
-/*     case LAYER_META: */
-/*         rgblight_setrgb (0xFF,  0x00, 0x00); */
-/*         break; */
-/*     default: //  for any other layers, or the default layer */
-/*         rgblight_setrgb (0x00,  0xFF, 0xFF); */
-/*         break; */
-/*     } */
-/*   return state; */
-/* } */
